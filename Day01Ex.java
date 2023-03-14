dfd6689ecad1a180f23efd9284e902675432e7c8
Kim Dong Hyun <127280994+coffeegoodzz@users.noreply.github.com>
2023-03-15 01:08:49 +0900

    Add files via upload
    깃허브 처음 사용 테스트입니다.

---
+++ Day01Ex.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class Day01Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * System.out.println("문자열값"); //문자열값 System.out.println('A'); //문자형값
+		 * System.out.println(100); //정수형값 System.out.println(3.14); //실수형값
+		 */
+		
+    		}
+
+}
